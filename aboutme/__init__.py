80bb932d0bca3351e76388bc16548ee4eeeac615
rushan <ozdan@inbox.ru>
2014-12-03 02:07:30 +0300

    Added new views

--- aboutme/__init__.py
+++ aboutme/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from aboutme import views
 
 from .models import (
     DBSession,
@@ -18,5 +19,11 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.scan()
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.add_route('register', '/register')
+    config.add_route('user', '/{username}')
+    config.add_route('users', '/users')
+    config.add_route('guests', '/guests')
+    config.scan(views)
     return config.make_wsgi_app()
--- aboutme/views.py
+++ aboutme/views.py
@@ -1,4 +1,8 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from paste.httpexceptions import HTTPFound
 from pyramid.response import Response
+from pyramid.security import forget, remember
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
@@ -34,3 +38,54 @@ After you fix the problem, please restart the Pyramid application to
 try it again.
 """
 
+
+@view_config(route_name='login')
+def login(request):
+    login_url = request.route_url('login')
+    referrer = request.url
+    if referrer == login_url:
+        referrer = '/'
+    came_from = request.params.get('came_from', referrer)
+    if 'form.submitted' in request.POST:
+        login = request.params.get('login')
+        password = request.params.get('password')
+        if False:  # Find in DB
+            headers = remember(request, login)
+            return HTTPFound(location=came_from, headers=headers)
+        message = "Неправильный логин или пароль"
+
+    return dict(
+        message=message,
+        url=login_url,
+        came_from=came_from,
+        login=login,
+        password=password
+    )
+
+
+@view_config(route_name='logout')
+def logout(request):
+    headers = forget(request)
+    url = request.route_url('home')
+    return HTTPFound(location=url, headers=headers)
+
+
+@view_config(route_name='register')
+def register(request):
+    return Response('OK')
+
+
+@view_config(route_name='user')
+def user(request):
+    username = request.matchdict.get('username') or None
+    return Response('OK')
+
+
+@view_config(route_name='users')
+def users(request):
+    return Response('OK')
+
+
+@view_config(route_name='guests')
+def guests(request):
+    return Response('OK')
