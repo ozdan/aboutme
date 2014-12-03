3069895260d2af900778b59abc53aa49394445aa
rushan <ozdan@inbox.ru>
2014-12-04 00:38:38 +0300

    Updated model User

--- aboutme/models.py
+++ aboutme/models.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from sqlalchemy import (
     Column,
     Index,
     Integer,
     Text,
+    String,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -18,10 +21,19 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    first_name = Column(String(32))
+    last_name = Column(String(32))
+    biography = Column(Text)
+    location = Column(String(50))
+    work = Column(String(50))
+    education = Column(String(70))
+    interest = Column(String(30))
+
+    username = Column(String(32))
+    email = Column(String(64))
+    password = Column(Text)
 
 # Index('my_index', MyModel.name, unique=True)
